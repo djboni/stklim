9e64d31b984c97beee2e08af7d749369e371be31
Djones A. Boni <djboni@gmail.com>
2016-11-12 17:59:56 -0200

    Stack usage

---
+++ stklim.c
@@ -0,0 +1,73 @@
+/*
+ Copyright 2016 Djones A. Boni
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#include "stklim.h"
+
+/*
+ To change the stack size:
+ 1) Change STACK_SIZE definition.
+ 2) Change .stklim section start.
+
+ Linker option to set .stklim section start:
+ AVR ATmega328P (256 bytes) -Wl,-section-start=.stklim=0x800800
+ */
+
+#ifndef STACK_SIZE
+/** Stack size in bytes. Should be multiple of 4. */
+#define STACK_SIZE 256
+#endif
+
+#ifndef STACK_FILL_VALUE
+/** Value to initialize the stack. Avoid common numbers such as 00s and FFs. */
+#define STACK_FILL_VALUE 0xFA57C0DEUL
+#endif
+
+/* Number of 32bit words in the stack. */
+#define STACK_SIZE_32 (STACK_SIZE / 4)
+
+/* Stack memory. Should not be modified after initialization. */
+__attribute__((section(".stklim")))
+static volatile uint32_t stackMemory[STACK_SIZE_32];
+
+/* Initialize stack with a constant 32bit value. Should not be called by the
+ user. */
+__attribute__((section(".init3")))
+__attribute__((naked))
+void stackInit(void)
+{
+    int16_t i;
+    for(i = STACK_SIZE_32 - 1; i >= 0; --i)
+        stackMemory[i] = STACK_FILL_VALUE;
+}
+
+/** Check the two last 32bit words of the stack as a fast overflow test. Returns
+1 if at least one word was changed, 0 otherwise. */
+int8_t stackOverflow(void)
+{
+    return  stackMemory[1] != STACK_FILL_VALUE ||
+            stackMemory[0] != STACK_FILL_VALUE;
+}
+
+/** Check the number of 32bit words of the stack that was not changed. Returns
+the number of words of the stack that were not used. */
+int16_t stackUsage(void)
+{
+    int16_t i;
+    for(i = 0; i < STACK_SIZE_32; ++i)
+        if(stackMemory[i] != STACK_FILL_VALUE)
+            break;
+    return i;
+}
